428e2e5793661bfc04f5417b530ace52a030693b
sharon <sapot@safaricom.co.ke>
2023-11-13 13:55:42 +0300

    Add ArticleLists

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 import blogData from "../data/blog";
 
 console.log(blogData);
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header name={blogData.blogName} />
       <About image={blogData.about.image} aboutText={blogData.about.text} />
+      <ArticleList posts={posts} />
     </div>
   );
 }
